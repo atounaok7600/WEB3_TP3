08c8846cafdaccd38c4d0005bfe3471435b28fc6
abdel <atounaok@gmail.com>
2023-11-10 10:32:21 -0500

    Style de la barre de navigation

--- garenoticket/src/components/Navbar.vue
+++ garenoticket/src/components/Navbar.vue
@@ -3,12 +3,16 @@
 </script>
 
 <template>
-    <header class="border">
-        <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="50" height="50" />
+    <header class="border flex justify-between items-center py-2 px-5">
+        <RouterLink to="/">
+            <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="50" height="50" />
+        </RouterLink>
 
-        <nav>
-            <RouterLink to="/">Home</RouterLink>
-            <RouterLink to="/about">About</RouterLink>
+        <nav class="flex gap-2">
+            <RouterLink to="/" class="py-2 px-4 hover:bg-slate-200">Accueil</RouterLink>
+            <RouterLink to="/login" class="py-2 px-4 hover:bg-slate-200">Se connecter</RouterLink>
+            <RouterLink to="/signup" class="py-2 px-4 hover:bg-slate-200">Créer un compte</RouterLink>
+            <RouterLink to="/about" class="py-2 px-4 hover:bg-slate-200">About</RouterLink>
         </nav>
     </header>
 </template>
