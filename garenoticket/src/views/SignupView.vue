db94b6804062fbc3f58fede97d2b036c011fc302
abdel <atounaok@gmail.com>
2023-11-10 11:10:35 -0500

    Avancements sur le formulaire de création

--- garenoticket/src/views/SignupView.vue
+++ garenoticket/src/views/SignupView.vue
@@ -3,12 +3,13 @@
 </script>
 
 <template>
-    <div class="h-screen flex justify-center items-center">
-        <Form class="py-6 px-10 border rounded-lg shadow-md flex flex-col gap-4">
-            <div class="flex items-center justify-center">
-                <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <div class=" min-h-[100vh] flex justify-center items-center">
+        <Form class="py-6 px-10 border rounded-lg shadow-md flex flex-col gap-8">
+            <div class="flex flex-col items-center justify-center">
+                <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="120" height="120" />
+                <h2 class="text-5xl">Créer un compte</h2>
             </div>
-            
+
             <div class="flex flex-col gap-4">
                 <div class="flex flex-col gap-1">
                     <label for="nom"><strong>Nom</strong></label>
@@ -31,7 +32,7 @@
                 </div>
                 <div class="flex justify-between gap-4">
                     <input type="submit" class="border w-full py-2 px-4 bg-orange-400 hover:cursor-pointer hover:bg-orange-500">
-                    <input type="reset" class="border w-full py-2 px-4 bg-slate-400 hover:cursor-pointer hover:bg-slate-500">
+                    <input type="reset" class="border w-full py-2 px-4 bg-slate-200 hover:cursor-pointer hover:bg-slate-300">
                 </div>
             </div>
         </Form>
