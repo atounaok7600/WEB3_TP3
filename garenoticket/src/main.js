432a3056ad9108b7557046338dc66a5aaf23e383
abdel <atounaok@gmail.com>
2023-11-14 02:28:16 -0500

    retrait du mauvais toat

--- garenoticket/src/main.js
+++ garenoticket/src/main.js
@@ -3,11 +3,9 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import Toast from 'vue-toastification';
 
 const app = createApp(App)
 
 app.use(router)
-app.use(Toast)
 
 app.mount('#app')
