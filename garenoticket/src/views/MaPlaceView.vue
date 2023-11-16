629466720152a2903b8f8d759e308fe92a73c144
abdel <atounaok@gmail.com>
2023-11-16 12:44:05 -0500

    Amélioration de ma place view

--- garenoticket/src/views/MaPlaceView.vue
+++ garenoticket/src/views/MaPlaceView.vue
@@ -1,132 +1,46 @@
-<script>
-  import { useRouter } from 'vue-router';
-  import { ref, onMounted } from 'vue';
-  import AppLayout from '../layouts/AppLayout.vue'
-  import leaflet, { marker } from 'leaflet'
-  import L from 'leaflet'
-  import mapMarkerRed from '../assets/map-marker-red.svg';
+<script setup>
+import AppLayout from '../layouts/AppLayout.vue';
+import { ref, onMounted } from 'vue'
+import L from 'leaflet'
 
-  export default {
-    components: {
-    AppLayout,
-},
-    setup() {
-      let marker;
-      const map = ref()
-      const mapContainer = ref()
-      let isParked = ref(false);
+  const coords = {
+    lat: ref(0),
+    lng: ref(0),
+  }
+  const map = ref()
+  const mapContainer = ref()
 
-      onMounted(() => {
-        map.value = L.map(mapContainer.value, {zoomAnimation: false}).setView([51.505, -0.09], 13);
+  onMounted(() => {
+    // Obtenir la localisation du user
+    getLocation();
 
-        //tuiles
-        leaflet.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(map);
+    // Initialiser la carte
+    map.value = L.map(mapContainer.value).setView([51.505, -0.09], 13);
 
-        getGeolocation();
+    // Tuiles de la carte
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map.value);
 
-        map.on('click', onMapClick);
+  });
 
-        marker = leaflet.marker([51.505, -0.09], { draggable: !isParked.value }).addTo(map);
+  // Permet de demander la localisation du user.
+  const getLocation = () => {
+    if(navigator.geolocation){
+      navigator.geolocation.watchPosition((pos) => {
+        coords.lat.value = pos.coords.latitude
+        coords.lng.value = pos.coords.longitude
+        map.value.setView([coords.lat.value, coords.lng.value, 13]);
 
-        marker.on('dragend', () => {
-          coords.value = {
-            lat: e.target.getLatLng().lat,
-            lng: e.target.getLatLng().lng,
-          }
-        })
-
-      });
-
-      const coords = ref(null);
-      const fetchCoords = ref(null);
-      const geoMarker = ref(null);
-
-      const getGeolocation = () => {
-        if(sessionStorage.getItem('coords')){
-          coords.value = JSON.parse(sessionStorage.getItem('coords'));
-          plotGeolocation(coords.value);
-          return;
-        }
-
-        fetchCoords.value = true;
-        navigator.geolocation.getCurrentPosition(setCoords, getLocError);
-      };
-
-      const onMapClick = (e) => {
-        coords.value = {
-          lat: e.latlng.lat,
-          lng: e.latlng.lng,
-        }
-
-        if(geoMarker.value){
-          map.removeLayer(geoMarker.value)
-        }
-
-        plotGeolocation(coords.value)
-
-      }
-
-      const setCoords = (pos) => {
-        fetchCoords.value = null;
-        
-        const setSessionCoords = {
-          lat: pos.coords.latitude,
-          lng: pos.coords.longitude,
-        };
-
-        sessionStorage.setItem('coords', JSON.stringify(setSessionCoords));
-
-        coords.value = setSessionCoords;
-
-        plotGeolocation(coords.value);
-      };
-
-      const getLocError = (err) => {
-        console.log(err);
-      }
-
-      const plotGeolocation = () => {
-        if (map){
-          //create custom marker
-          const customMarker = leaflet.icon({
-            iconUrl: mapMarkerRed,
-            iconSize: [35, 35],
-          });
-
-          if(geoMarker.value){
-            map.removeLayer(geoMarker.value);
-          }
-
-          geoMarker.value = leaflet.marker([coords.value.lat, coords.value.lng], {icon: customMarker})
-            .addTo(map);
-
-          const currentZoom = map.getZoom();
-          map.setView([coords.value.lat, coords.value.lng], currentZoom);
-        }
-      };
-
-      const startParking = () => {
-        isParked.value = true;
-        marker.dragging.disable();
-      };
-
-      const confirmParking = () => {
-        // Enregistrer les coordonnées actuelles et l'heure de stationnement dans la base de données
-        isParked.value = true;
-        marker.dragging.disable();
-        // Enregistrer les coordonnées et l'heure dans la base de données
-      }
-
-      return {
-        coords,
-        geoMarker,
-        startParking,
-        confirmParking,
-      };
-    },
+        // Ajour du marqueur
+        L.marker([coords.lat.value, coords.lng.value], { draggable: true })
+        .addTo(map.value)
+        .on('dragend', (e) => {
+          console.log(e);
+        });
+      })
+    }
   }
 </script>
 
